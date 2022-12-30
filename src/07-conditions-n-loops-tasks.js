e84f214745aaa973daed1526b7cc8a64ff4460af
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2022-12-30 06:36:46 +0300

    feat: write functions named doRectanglesOverlap and isInsideCircle

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -139,47 +139,14 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  /*
-  if ((rect1.top < rect2.top < (rect1.top + rect1.height))
-  && (rect1.left < rect2.left < (rect1.left + rect1.width))) {
-    return true;
+function doRectanglesOverlap(rect1, rect2) {
+  if ((rect2.top + rect2.height) < rect1.top
+  || (rect2.left + rect2.width) < rect1.left
+  || rect2.top > (rect1.top + rect1.height)
+  || rect2.left > (rect1.left + rect1.width)) {
+    return false;
   }
-  if ((rect1.top < (rect2.top + rect2.height) < (rect1.top + rect1.height))
-  && (rect1.left < rect2.left < (rect1.left + rect1.width))) {
-    return true;
-  }
-
-  if ((rect1.top < (rect2.top + rect2.height) < (rect1.top + rect1.height))
-  && (rect1.left < (rect2.left + rect2.width) < (rect1.left + rect1.width))) {
-    return true;
-  }
-  if ((rect1.top < rect2.top < (rect1.top + rect1.height))
-  && (rect1.left < (rect2.left + rect2.width) < (rect1.left + rect1.width))) {
-    return true;
-  }
-
-  if ((rect2.top < rect1.top < (rect2.top + rect2.height))
-  && (rect2.left < rect1.left < (rect2.left + rect2.width))) {
-    return true;
-  }
-  if ((rect2.top < (rect1.top + rect1.height) < (rect2.top + rect2.height))
-  && (rect2.left < rect1.left < (rect2.left + rect2.width))) {
-    return true;
-  }
-
-  if ((rect2.top < (rect1.top + rect1.height) < (rect2.top + rect2.height))
-  && (rect2.left < (rect1.left + rect1.width) < (rect2.left + rect2.width))) {
-    return true;
-  }
-  if ((rect2.top < rect1.top < (rect2.top + rect2.height))
-  && (rect2.left < (rect1.left + rect1.width) < (rect2.left + rect2.width))) {
-    return true;
-  }
-
-  return false;
-  */
-  throw new Error('Not implemented');
+  return true;
 }
 
 
@@ -209,8 +176,13 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const differ = Math.sqrt((point.x - circle.center.x) ** 2 + (point.y - circle.center.y) ** 2);
+  if (differ < circle.radius) {
+    return true;
+  }
+
+  return false;
 }
 
 
